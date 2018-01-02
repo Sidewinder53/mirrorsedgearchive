ada33d66681ce086bd758c6b254cc6034a91a894
Tobias Kleinmann <19623985+Sidewinder53@users.noreply.github.com>
2018-01-03 00:36:44 +0100

    Improved credits page scrolling and changed images

--- resources/js/credits.js
+++ resources/js/credits.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
 
+	if(is_touch_device()==true) {
+		$('#touch_device_notice').html('<div class="alert alert-primary" role="alert"><mark>Highlighted</mark> staff has been credited in both titles.</div>');
+		$('#touch_device_notice').animate({height:100},"slow").fadeTo("slow", 1);
+	}
+
 	$(window).on('activate.bs.scrollspy', function () {
 		updateBackground();
 	});
@@ -8,21 +13,21 @@ $( document ).ready(function() {
 		viewport: null,
 		styles: {
 		'header,footer,section,article': 'rgba(0,0,0,0.08)',
-		'th': 'rgba(255,0,0,0.10)',
-		'h1': 'rgba(255,0,0,0.8)',
-		'.alert': 'rgba(0,0,0,0.3)',
-		'img': 'rgba(130,0,0,0.3)',
-		'h2,h3,h4,tr': 'rgba(0,0,0,0.03)',
+		'th': 'rgba(255,0,0,0.35)',
+		'h1': 'rgba(255,0,0,0.9)',
+		'.alert': 'rgba(0,0,0,0.4)',
+		'.art_space > img': 'rgba(255,0,0,0.08)',
+		'tr': 'rgba(0,0,0,0.03)',
 		},
-		back: 'rgba(0,0,0,0.02)',
+		back: '#f8f9fa',
 		view: 'rgba(0,0,0,0.05)',
 		drag: 'rgba(0,0,0,0.10)',
 		interval: null
 	});
 
-	$(".dropdown-item, .nav-click").click(function(e) {
+	$(".int-nav-menu, .int-nav").click(function(e) {
 	e.preventDefault();
-	anchorScroll( $(this), $($(this).attr("href")), 100 );
+	anchorScroll( $(this), $($(this).attr("href")), 150 );
 	});
 
 	function anchorScroll(this_obj, that_obj, base_speed) {
@@ -30,20 +35,28 @@ $( document ).ready(function() {
 		var that_offset = that_obj.offset();
 		var offset_diff = Math.abs(that_offset.top - this_offset.top + 56);
 
-		var speed       = (offset_diff * base_speed) / 1000;
+		var speed       = (offset_diff * base_speed) / 900;
 
 		$("html,body").animate({
 			scrollTop: that_offset.top - 56
-		}, speed);
+		}, {
+			duration: speed,
+			easing: "easeOutCubic"
+		});
 	}
 
 	function updateBackground() {
-		if ($('#me_dice_nav').hasClass('active') || $('#me_va_nav').hasClass('active') || $('#me_ea_nav').hasClass('active') || $('#home_nav').hasClass('active')) {
-			$('#bg').css("background-size", "0");
-			$('body').css("background-size", "12%");
-		} else {
-			$('#bg').css("background-size", "20%");
-			$('body').css("background-size", "0");
+		if ( $('.me-nav-area').hasClass('active') && $('#cd_me').css('opacity')<'1' ) {
+			$('#cd_mec').fadeTo("fast", 0);
+			$('#cd_me').fadeTo("medium", 1);
+		} else if (!$('.me-nav-area').hasClass('active') && $('#cd_mec').css('opacity')<'1') {
+			$('#cd_me').fadeTo("fast", 0);
+			$('#cd_mec').fadeTo("medium", 1);
 		}
 	}
+
+	function is_touch_device() {
+		return 'ontouchstart' in window
+		|| navigator.maxTouchPoints;
+	};
 });
