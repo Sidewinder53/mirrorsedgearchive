3d84fab5f63f29478a90a6052b76437af36a0f76
Tobias Kleinmann <19623985+Sidewinder53@users.noreply.github.com>
2018-02-04 16:33:21 +0100

    Updated video posters

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
+	var skipped = 'false';
+	if(Cookies.get('skip_intro') =='true') {
+		skipped = 'true';
+		$("#hero_text, #overview, .navbar").fadeTo( "slow" , 1);
+		$(".card").css("border-width", "1px");
+	}
 	setTimeout(function(){
 		if(($("#hero").get(0).paused) == true) {
 			$("#hero").css("opacity", "0");
-			$("#hero").attr( 'poster', 'resources/img/homepage_banner.jpg');
+			//$("#hero").attr( 'poster', 'resources/img/homepage_banner.jpg');
 			$("#hero_text, #overview, .navbar, #hero").fadeTo( "slow" , 1);
 			$(".card").css("border-width", "1px");
 		}
 	}, 750);
 	$("#hero").on("timeupdate", function(event){
-		if(this.currentTime >= 1.5) {
+		if(this.currentTime >= 1.5 && skipped == 'false') {
 			$("#hero_text, #overview, .navbar").fadeTo( "slow" , 1);
 			$(".card").css("border-width", "1px");
+			Cookies.set("skip_intro", 'true', { expires: 1/24 });
 		}
 	});
 	$('video').on('ended',function(){
