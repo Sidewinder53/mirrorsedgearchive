d8a4cb93c3e36d11bdd75b75aacf60e72cc8c25b
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2018-07-23 18:03:24 +0200

    Changed handling of PHP request for project graffiti

--- dev/project_graffiti/generate.php
+++ dev/project_graffiti/generate.php
@@ -54,24 +54,25 @@ if ($version < "2") {
 	die("deprecated");
 }
 
-$ch = curl_init('https://mirrorsedgearchive.de/project_graffiti/a7e93kdfn0uztu8r6/mectagmaker.php');
+$bgXY = $bgX.'x'.$bgY;
 
-$post = [
-	'emblem'		=> $emblem,
-	'frame'			=> $frame,
-	'background'	=> $background,
-	'bgX'			=> $bgX,
-	'bgY'			=> $bgY,
-	'em'			=> $em,
-	'unused'		=> $unused,
-	'passphrase'	=> "D7xIw857G057WrWLNzeb"
-];
+$random = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),0, 1) . substr(str_shuffle('aBcEeFgHiJkLmNoPqRstUvWxYz0123456789'),0, 10);
 
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-echo $result;
-curl_close($ch);
+$old_path = getcwd();
+chdir('/var/www/mirrorsedgearchive_de/www/mectagmaker');
+exec("./mectagmaker.sh $emblem $frame $background $random bgresize=$bgXY emresize=$em unused=$unused");
+chdir($old_path);
+//$im = imagecreatefrompng("/dev/shm/mectagmaker-output-$random.png");
+
+//Posting image to uguu.se
+$ch = curl_init('https://uguu.se/api.php?d=upload-tool');
+$file = new CURLFile("/dev/shm/mectagmaker-output-$random.png", 'image/png', '/dev/shm/mectagmaker-output.png');
+$data = array('file' => $file);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$response = curl_exec($ch);
+$exturl=rtrim($response, '1');
+echo $exturl;
+//exec("rm /dev/shm/mectagmaker-output-$random.png");
+exit();
 ?>
