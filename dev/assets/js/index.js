6e4bbec8fbebbbe1635d05558821016802692533
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2018-08-04 18:15:16 +0200

    Carousel on home displays a random selection of images

--- dev/assets/js/index.js
+++ dev/assets/js/index.js
@@ -1,8 +1,38 @@
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 $(document).ready(function() {
   var skipped = 'false';
 
   if ($('#responsiveHero').css('display') != 'none') {
     $('#hero')[0].play();
+    var cit =
+      '<div class="carousel-item"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_X.webp" type="image/webp"><img src="/assets/media/image/home/hero_img_X.jpg"></picture></div>';
+    var cis =
+      '<div class="carousel-item active"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_1.webp" type="image/webp"><img src="/assets/media/image/home/hero_img_1.jpg"></picture></div>';
+    enu = shuffle([2, 3, 4, 5, 6, 7, 8, 9]);
+    for (i = 0; i < 4; i++) {
+      cis += cit.replace(/hero_img_X/g, 'hero_img_' + enu[i]);
+    }
+
+    $('#heroSlides > .carousel-inner').html(cis);
     $.getScript('/assets/js/cookie-consent.js', function() {
       if (Cookies.get('skip_intro') == 'true') {
         skipped = 'true';
@@ -40,7 +70,7 @@ $(document).ready(function() {
   });
 
   $('video').on('ended', function() {
-    $('#heroSlides').carousel(0);
+    $('#heroSlides').carousel({ interval: 3000 });
     $('#hero').addClass('d-none');
     $('#heroSlides').removeClass('d-none');
     $('#media_copyright').fadeTo('slow', 1);
