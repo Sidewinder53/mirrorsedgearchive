7b666fe4e59987d282554a0eb6c31f09f4540c49
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2019-06-01 03:33:10 +0200

    First test build, only homepage functional

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
+import Cookies from 'cookies.js';
+import 'bootstrap';
+
 function shuffle(a) {
   for (var i = a.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -21,14 +25,14 @@ $(document).ready(function() {
 
   if ($("#responsiveHero").css("display") != "none") {
     $("#hero")[0].play();
-    var cit = '<div class="carousel-item"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_X.webp" type="image/webp"><img src="/assets/media/image/home/hero_img_X.jpg"></picture></div>';
-    var cis = '<div class="carousel-item active"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_1.webp" type="image/webp"><img src="/assets/media/image/home/hero_img_1.jpg"></picture></div>';
-    enu = shuffle([2, 3, 4, 5, 6, 7, 8, 9]);
-    for (i = 0; i < 4; i++) {
-      cis += cit.replace(/hero_img_X/g, "hero_img_" + enu[i]);
-    }
+    // var cit = '<div class="carousel-item"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_X.webp" type="image/webp"><img src="/assets/media/image/home/hero_img_X.jpg"></picture></div>';
+    // var cis = '<div class="carousel-item active"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_1.webp" type="image/webp"><img src="/assets/media/image/home/hero_img_1.jpg"></picture></div>';
+    // var enu = shuffle([2, 3, 4, 5, 6, 7, 8, 9]);
+    // for (i = 0; i < 4; i++) {
+    //   cis += cit.replace(/hero_img_X/g, "hero_img_" + enu[i]);
+    // }
 
-    $("#heroSlides > .carousel-inner").html(cis);
+    $("#heroSlides > .carousel-inner").html('<div class="carousel-item active"><picture class="d-block w-100"><source srcset="/assets/media/image/home/hero_img_1.jpg" type="image/jpg"><img src="/assets/media/image/home/hero_img_1.jpg"></picture></div>');
     $.getScript("/assets/js/cookie-consent.js", function() {
       if (Cookies.get("skip_intro") == "true") {
         skipped = "true";
