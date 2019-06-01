f48f0af9619fdf94b2ad2e1a51a4738cd9714473
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2019-06-01 13:33:50 +0200

    Further improvements to enable parcel bundler

---
+++ imagemin.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
+  mozjpeg: { progressive: true, quality: 10 },
+  pngquant: { quality: 10 },
+  svgo: {
+    plugins: [{ removeViewBox: false }, { cleanupIDs: true }]
+  },
+  webp: { quality: 10 }
+};
