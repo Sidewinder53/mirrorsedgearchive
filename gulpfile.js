7fab7c96cba0ed4fecc45f3a097bd6a024a1f988
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2020-03-13 20:56:22 +0100

    Added an integrated version of the main archive

--- gulpfile.js
+++ gulpfile.js
@@ -70,6 +70,7 @@ function processTemplateDev() {
     './src/project_doomsday/index.html',
     './src/project_graffiti/index.html',
     './src/archive/index.html',
+    './src/archive/integrated.html',
     './src/contribute/index.html',
     './src/credits/index.html',
     './src/legal/*.html'
